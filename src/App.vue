edfd73f3c3da16bea65ff81db8b059eeb1f547da
seriouslysean <sean@codeserious.com>
2023-06-14 09:27:06 -0400

    Add additional information

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,15 @@ function onReadyPlayer(player) {
     return;
   }
   player.ready = true;
+
+  // TODO
+  // There can be a poll here to check if the other player is ready every second, in which case
+  // we would use a setInterval instead of a timeout and just query the game status
+  // something like GET `/api/game/:id/rematch` which would send back the rematch status
+  //     - Whether the players are ready, or if the game is no longer valid, etc
+  //     - If the game is no longer valid, we can use that to trigger cleanup and either show
+  //       a message to the user, or send them back to the lobby then show a message
+
   player.timerId = setTimeout(() => resetTimeout(player), TIMEOUT_PLAYER_READY);
 }
 
@@ -64,6 +73,36 @@ function onReset() {
   <div class="lobby">
     <header class="header">
       <h1>Cuttle: Async Rematch Logic</h1>
+      <p>
+        This demo is to show how to manage the game start logic between two
+        discrete clients. There are a few issues to keep in mind, though.
+      </p>
+      <ol>
+        <li>Both players need to be ready before the game starts</li>
+        <li>We need some way for client to be aware of the player readiness</li>
+        <li>
+          There needs to be some way to let the other player know that the game
+          is starting
+        </li>
+      </ol>
+      <p>
+        To solve this, we need to use some sort of timeout in combination with
+        either a check (or poll) to a data store to see if the other player is
+        ready when we are, or to listen for a websocket event to notify when the
+        game is ready.
+      </p>
+      <ol>
+        <li>
+          For a timeout, the logic can be done inside of the
+          <code>onReadyPlayer</code> function
+        </li>
+        <li>
+          If using websockets, there would need to be a socket listener for game
+          ready via id but there's the potential the current user might miss the
+          blast if they are disconnected.
+        </li>
+      </ol>
+      <p>It's likely safer to just check the database on an interval.</p>
     </header>
     <section class="player player--one">
       <h2>Player 1</h2>
